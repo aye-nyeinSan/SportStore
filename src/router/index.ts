4b50d4b14225b60184732e4e9bcbefc8fb757588
Ruth <hyangdan0715@gmail.com>
2024-10-30 00:06:15 +0700

    add basket.vue

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import RegisterView from '@/views/RegisterForm.vue'
 import LoginView from '@/views/LoginForm.vue'
+import BasketView from '@/views/BasketView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -28,6 +29,11 @@ const router = createRouter({
       path:'/login',
       name:'login',
       component:LoginView
+    },
+    {
+      path:'/basket',
+      name:'basket',
+      component:BasketView
     }
   ],
 })
---
+++ src/views/BasketView.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-container fluid class="pa-8">
+    <v-row>
+      <!-- Cart Items Section -->
+      <v-col cols="12" md="8">
+        <CartItem v-for="n in 3" :key="n" />
+      </v-col>
+
+      <!-- Order Summary Section -->
+      <v-col cols="12" md="4">
+        <OrderSummary />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import CartItem from "@/components/basketCard.vue";
+import OrderSummary from "@/components/orderSummary.vue";
+
+export default {
+  name: "Check-out",
+  components: {
+    CartItem,
+    OrderSummary,
+  },
+};
+</script>
