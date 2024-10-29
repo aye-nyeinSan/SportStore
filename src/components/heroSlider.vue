8f5ff965bae86fed91c879171941f00189897800
aye-nyeinSan <aye2904@gmail.com>
2024-10-29 19:14:13 +0700

    add slider

---
+++ src/components/heroSlider.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-app>
+    <v-container>
+      <v-carousel show-arrows>
+        <template v-slot:prev="{ props }">
+          <v-btn
+            variant="elevated"
+            @click="props.onClick"
+            :style="{ backgroundColor: 'transparent', boxShadow: 'none',color: 'white'  }"
+          >
+            <
+          </v-btn>
+        </template>
+        <template v-slot:next="{ props }">
+          <v-btn
+            variant="elevated"
+            @click="props.onClick"
+            :style="{ backgroundColor: 'transparent', boxShadow: 'none',color: 'white' }"
+          >
+            >
+          </v-btn>
+        </template>
+        <v-carousel-item
+          src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+          cover
+        ></v-carousel-item>
+
+        <v-carousel-item
+          src="https://cdn.vuetifyjs.com/images/cards/hotel.jpg"
+          cover
+        ></v-carousel-item>
+
+        <v-carousel-item
+          src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+          cover
+        ></v-carousel-item>
+      </v-carousel>
+    </v-container>
+  </v-app>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import HeroSlider from '@/components/heroSlider.vue';
   import Why from '@/components/whyShop.vue'
   import promo from '@/assets/4.png'
 </script>
@@ -6,13 +7,14 @@
 <template>
   <main>
     <v-container>
-      
+      <HeroSlider/>
       <div class="promotion">
         <img :src="promo" alt="" 
         :width="100"
         width="100%"
         aspect-ratio="16/9">
       </div>
+      
       <Why/>
      
      <div class="links">
