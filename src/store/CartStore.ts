f2cb96b0ace798488c45c227052d1a6f31b0b7c3
aye-nyeinSan <aye2904@gmail.com>
2024-10-30 15:06:06 +0700

    add t cart dynamic works

--- src/store/CartStore.ts
+++ src/store/CartStore.ts
@@ -2,14 +2,30 @@ import { defineStore } from 'pinia'
 
 export const useCartStore = defineStore('cart', {
   state: () => ({
-    cart: [] as any[], // Define state explicitly
+    cart: [] as any[],
   }),
+  getters: {
+    cartItemCount: state => state.cart.length,
+  },
   actions: {
     addToCart(item: any) {
-      this.cart.push(item)
+      const existingItem = this.cart.find(cartItem => cartItem.id === item.id)
+      if (existingItem) {
+        existingItem.quantity += item.quantity
+      } else {
+        this.cart.push({ ...item, quantity: item.quantity })
+      }
     },
-    getCartItems() {
-      return this.cart
+    incrementItemQuantity(id: number) {
+      const item = this.cart.find(cartItem => cartItem.id === id)
+      if (item) item.quantity++
+    },
+    decrementItemQuantity(id: number) {
+      const item = this.cart.find(cartItem => cartItem.id === id)
+      if (item && item.quantity > 1) item.quantity--
+    },
+    removeFromCart(id: number) {
+      this.cart = this.cart.filter(item => item.id !== id)
     },
   },
 })
