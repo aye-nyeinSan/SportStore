fe5b1d88dd73d6147f7d719aba6c511ed1ed032e
aye-nyeinSan <aye2904@gmail.com>
2024-10-30 17:34:42 +0700

    add delete in add to cart

--- src/components/basketCard.vue
+++ src/components/basketCard.vue
@@ -1,4 +1,3 @@
-
 <script lang="ts">
 import SvgIcon from "@jamescoyle/vue-icon";
 import { mdiMinus } from "@mdi/js";
@@ -23,6 +22,13 @@ export default defineComponent({
   setup(props) {
     const cartStore = useCartStore();
 
+   const deleteItem = () => {
+  console.log("Deleting item:", props.item);
+  cartStore.removeFromCart(props.item.id);
+  console.log("Updated cart:", cartStore.cartItems);
+};
+
+
     const incrementQuantity = () => {
       cartStore.incrementItemQuantity(props.item.id);
     };
@@ -31,49 +37,42 @@ export default defineComponent({
       cartStore.decrementItemQuantity(props.item.id);
     };
 
-    const removeItem = () => {
-      cartStore.removeFromCart(props.item.id);
-    };
-
-    return { incrementQuantity, decrementQuantity, removeItem };
+    return { incrementQuantity, decrementQuantity, deleteItem };
   },
 });
 </script>
+
 <template>
-    <v-card outlined class="mb-4">
-        <v-row no-gutters class="align-center">
-        <v-col cols="2">
-            <v-img :src="item.image" alt="Product Image" class="rounded" max-width="100"></v-img>
-        </v-col>
+  <v-card outlined class="mb-4">
+    <v-row no-gutters class="align-center">
+      <v-col cols="2">
+        <v-img :src="item.image" alt="Product Image" class="rounded" max-width="100" />
+      </v-col>
 
-        <!-- Product Details -->
-        <v-col cols="6">
-            <div class="d-flex flex-column">
-                <span class="font-weight-bold text-body-1">{{ item.name }}</span>
-                <span class="text-caption text--secondary">In stock and ready to ship</span>
-                <span class="text-caption text--secondary">Size: {{ item.selectedSize }}</span>
-            </div>
-        </v-col>
+      <v-col cols="6">
+        <div class="d-flex flex-column">
+          <span class="font-weight-bold text-body-1">{{ item.name }}</span>
+          <span class="text-caption text--secondary">In stock and ready to ship</span>
+          <span class="text-caption text--secondary">Size: {{ item.selectedSize }}</span>
+        </div>
+      </v-col>
 
-        <!-- Quantity Controls -->
-        <v-col cols="2" class="d-flex align-center justify-center">
-            <v-btn icon small @click="decrementQuantity">
-                <SvgIcon type="mdi" :path="mdiMinus"  />
-            </v-btn>
-            <span class="mx-2">{{ item.quantity }}</span>
-            <v-btn icon small @click="incrementQuantity">
-                <v-icon>mdi-plus</v-icon>
-            </v-btn>
-        </v-col>
+      <v-col cols="2" class="d-flex align-center justify-center">
+        <v-btn icon small @click="decrementQuantity">
+          <SvgIcon type="mdi" :path="mdiMinus" />
+        </v-btn>
+        <span class="mx-2">{{ item.quantity }}</span>
+        <v-btn icon small @click="incrementQuantity">
+          <v-icon>mdi-plus</v-icon>
+        </v-btn>
+      </v-col>
 
-        <!-- Price and Delete -->
-        <v-col cols="2" class="text-right">
-            <span class="font-weight-bold text-body-1">${{ item.price * item.quantity }}</span>
-            <v-btn icon small class="ml-2" color="error">
-                <v-icon>mdi-delete</v-icon>
-            </v-btn>
-        </v-col>
+      <v-col cols="2" class="text-right">
+        <span class="font-weight-bold text-body-1">${{ item.price * item.quantity }}</span>
+        <v-btn icon small class="ml-2" color="error" @click="deleteItem">
+          <v-icon>mdi-delete</v-icon>
+        </v-btn>
+      </v-col>
     </v-row>
-    </v-card>
+  </v-card>
 </template>
-
--- src/store/CartStore.ts
+++ src/store/CartStore.ts
@@ -2,33 +2,30 @@ import { defineStore } from 'pinia'
 
 export const useCartStore = defineStore('cart', {
   state: () => ({
-    cart: [] as any[],
+    cart: [] as any[], // Declare the cart state as an array
   }),
   getters: {
-    cartItemCount: state => state.cart.length,
+    cartItems: state => state.cart, // Reactive getter for cart items
   },
   actions: {
     addToCart(item: any) {
-      const existingItem = this.cart.find(cartItem => cartItem.id === item.id)
+      const existingItem = this.cart.find(i => i.id === item.id)
       if (existingItem) {
         existingItem.quantity += item.quantity
       } else {
-        this.cart.push({ ...item, quantity: item.quantity })
+        this.cart.push({ ...item }) // Add a new item reactively
       }
     },
+    removeFromCart(id: number) {
+      this.cart = this.cart.filter(item => item.id !== id) // Use filter to create a new array
+    },
     incrementItemQuantity(id: number) {
-      const item = this.cart.find(cartItem => cartItem.id === id)
+      const item = this.cart.find(i => i.id === id)
       if (item) item.quantity++
     },
     decrementItemQuantity(id: number) {
-      const item = this.cart.find(cartItem => cartItem.id === id)
+      const item = this.cart.find(i => i.id === id)
       if (item && item.quantity > 1) item.quantity--
-    },
-    removeFromCart(id: number) {
-      this.cart = this.cart.filter(item => item.id !== id)
-    },
-    clearCart() {
-      this.cart = [] // Clear all items from the cart
     },
   },
 })
